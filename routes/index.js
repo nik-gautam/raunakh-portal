394e72bf602eb38537cb29953a9509c809975571
Vidhi <vidhi152@gmail.com>
2020-10-26 22:05:45 +0530

    add socket io connections

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var server = require('http').createServer(express());
+var io = require('socket.io')(server); 
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { });
+});
+
+io.sockets.on("connection", (socket)=>{
+  socket.on("donation", (data)=>{
+    socket.broadcast.emit("donation-successfull", data);
+  });
 });
 
 module.exports = router;
