0f61d61f03fcb6ac23693ddab641705fb09658c5
Vidhi <vidhi152@gmail.com>
2020-10-28 17:21:10 +0530

    razorpay integration

---
+++ public/javascripts/payment.js
@@ -0,0 +1,27 @@
+let order_id = document.getElementById("order_id").value;
+let amount = document.getElementById("amount").value;
+
+console.log(order_id + " " + amount);
+
+var options = {
+  key: "rzp_test_nPAgDRmRtPGa4i",
+  amount: amount,
+  currency: "INR",
+  order_id: order_id,
+  callback_url: "http://localhost:3000/thanks",
+};
+
+let rzr = new Razorpay(options);
+
+rzr.on("payment.failed", function (response) {
+  alert(response.error.code);
+  alert(response.error.description);
+  alert(response.error.source);
+  alert(response.error.step);
+  alert(response.error.reason);
+  alert(response.error.metadata);
+});
+document.getElementById("razorpay-btn").onclick = function (e) {
+  rzr.open();
+  e.preventDefault();
+};
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,13 @@ var express = require('express');
 var router = express.Router();
 var server = require('http').createServer(express());
 var io = require('socket.io')(server); 
+var Razorpay = require("razorpay");
+
+let rzr = new Razorpay({
+  key_id: process.env.KEY_ID,
+  key_secret: process.env.KEY_SECRET,
+});
+
 
 
 /* GET home page. */
@@ -23,10 +30,91 @@ router.get("/logout", (req, res) => {
   res.redirect("/")
 })
 
-io.sockets.on("connection", (socket)=>{
-  socket.on("donation", (data)=>{
-    socket.broadcast.emit("donation-successfull", data);
+router.post("/payment", async (req, res, next) => {
+  console.log(req.body.amount);
+
+  var options = {
+    amount: parseInt(req.body.amount) * 100,
+    currency: "INR",
+  };
+
+  rzr.orders.create(options, function (err, order) {
+    if (!err) {
+      console.log(order);
+      res.render("payment", { order_id: order.id, amount: order.amount });
+    } else {
+      res.render("error", { error: err, message: "error aagaya bhai!!" });
+    }
   });
 });
+
+router.post("/thanks", async (req, res, next) => {
+  console.log(req.body);
+
+  // req.body will look like
+  // [Object: null prototype] {
+  //   razorpay_payment_id: 'pay_FuEZsb9wnYJSAL',
+  //   razorpay_order_id: 'order_FuEZT10FoiN40H',
+  //   razorpay_signature: '5b408cc7f17d666648cdf90955c7bcf2493f26324acc9e44b479361ed3937d7b'
+  // }
+
+  rzr.payments.fetch(req.body.razorpay_payment_id, (err, payment) => {
+    if (err) {
+      return res.render("error", {
+        error: err,
+        message: "error aagaya bhai!!",
+      });
+    }
+
+    // payment will look like this
+    // {
+    //   "id": "pay_FuEeZ4AxdXB83e",
+    //   "entity": "payment",
+    //*   "amount": 1000,
+    //   "currency": "INR",
+    //   "status": "captured",
+    //*   "order_id": "order_FuEe7dCSniLItk",
+    //   "invoice_id": null,
+    //   "international": false,
+    //*   "method": "upi",
+    //   "amount_refunded": 0,
+    //   "refund_status": null,
+    //   "captured": true,
+    //   "description": null,
+    //   "card_id": null,
+    //   "bank": null,
+    //   "wallet": null,
+    //   "vpa": "success@razorpay",
+    //*   "email": "nik@g.com",
+    //*   "contact": "+911236547890",
+    //   "notes": [],
+    //   "fee": 24,
+    //   "tax": 4,
+    //   "error_code": null,
+    //   "error_description": null,
+    //   "error_source": null,
+    //   "error_step": null,
+    //   "error_reason": null,
+    //   "acquirer_data": {
+    //   "rrn": "260510363608",
+    //   "upi_transaction_id": "1D496EC42BBBAB45AF546E2458A4C229"
+    //   },
+    //*   "created_at": 1603869351
+    //   }
+
+    res.render("thanks", {
+      email: payment.email,
+      amount: payment.amount,
+      paymentId: payment.id,
+    });
+  });
+});
+
+
+// io.sockets.on("connection", (socket)=>{
+//   socket.on("donation", (data)=>{
+//     socket.broadcast.emit("donation-successfull", data);
+//   });
+// });
 
 module.exports = router;
